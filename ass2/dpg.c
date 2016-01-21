2dd14a0375b9fa1cfe52db365b98a785f256d6cd
Siddharth Kannan <siddharthkannan@tutanota.com>
2016-01-21 13:23:53 +0530

    Setup working pipes
    Signed-off-by: Siddharth Kannan <siddharthkannan@tutanota.com>

--- ass2/dpg.c
+++ ass2/dpg.c
@@ -5,8 +5,11 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
-int n, k, prime_counter, *primearr, **pipes;
+#define BUFSIZE 100
+
+int n, k, prime_counter, self_process_counter, *primearr, **pipes;
 pid_t *process_id_arr;
 
 void print(char * str) {
@@ -31,6 +34,9 @@ int main(int argc, char ** argv) {
 	int i;
 	pid_t parent_pid = getpid(), pidc;
 	prime_counter = 0;
+	self_process_counter = -1; // parent is -1, children start indexing from 0
+	const char * hello_world = "Hello, world";
+	char buf[BUFSIZE];
 
 	// treat SIGUSR1 as available
 	// and SIGUSR2 as busy
@@ -68,15 +74,31 @@ int main(int argc, char ** argv) {
 	process_id_arr = (pid_t *) malloc(sizeof(pid_t) * k);
 
 	for(i = 0; i < k; ++i) {
-		pidc = fork();
 
 		// call pipe function here
-		/*pipe(pipes[i]);*/
+		if(pipe(pipes[i]) == -1) {
+			perror("pipe function did not work!");
+		}
+
+		pidc = fork();
 
 		if(pidc == 0) {
 			// inside child process
 
-			// setup the pipes here - TODO
+			// setup the pipes here
+			size_t nbyte;
+			if((nbyte = write(pipes[i][1], hello_world, strlen(hello_world))) == -1) {
+				perror("write function didn't work");
+			}
+
+			if(nbyte != 12) {
+				printf("[%d] Wrote %d chars", getpid(), (int)nbyte);
+				perror("write function only succeeded partially");
+			} else {
+				printf("[%d] wrote %d bytes\n", getpid(), (int)nbyte);
+			}
+
+			self_process_counter = i;
 
 			if(kill(parent_pid, SIGUSR1) == -1) {
 				perror("kill could not send the signal");
@@ -101,11 +123,26 @@ int main(int argc, char ** argv) {
 			printf("[%d] Child %d : %d\n", getpid(), i+1, process_id_arr[i]);
 		}
 
+		// read the hello world message from all children, to ensure pipe is working
+		for(i=0; i<k; ++i) {
+			read(pipes[i][0], buf, BUFSIZE);
+			printf("[%d] Recieved from child %d: %s\n", getpid(), i, buf);
+		}
+
 		// send k random numbers to each child - TODO
 	}
 
 	if(getpid() != parent_pid) {
 		// inside the child processes
+		print("This is inside the child process");
+	}
+
+	if(getpid() == parent_pid) {
+		printf("[%d] Closing all the pipes\n", getpid());
+		for(i=0; i<k; ++i) {
+			close(pipes[i][0]);
+			close(pipes[i][0]);
+		}
 	}
 
 	return 0;
